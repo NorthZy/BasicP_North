ced35aa22addfb348b9d145980f6ef9764c2bd22
Nathaphat Pinsuk <48213335+NorthZy@users.noreply.github.com>
2025-07-23 12:25:26 +0700

    Workshop

---
+++ Class3.py
@@ -0,0 +1,29 @@
+# 1. Nested Condition
+# hasRice = True
+# hasSpoon = False
+
+# if hasRice:
+#     print("มีข้าว")
+# else:
+#     print("กินช้อนไปละกันนะ")
+# if hasSpoon:
+#         print("มีช้อน")
+# else:
+#      print("มือจกไปเลยยยยย")
+
+# # 2. For 
+# box = 0
+# for i in range(1, 5, 1):
+#     print(box + i)
+#     box = box + i # การบวกเข้า box
+#     # box += i เป็น alternative
+
+# print("box =", box)
+
+# วนกี่รอบ = int(input("วนกี่รอบ :"))
+# sum = 0
+# for i in range(1, วนกี่รอบ + 1):
+#     sum = sum + i
+#     print("รอบที่", i , "ผลรวมคือ", sum)
+
+
---
+++ Class3_Workshop.py
@@ -0,0 +1,15 @@
+mon = 150
+wp1 = 10
+wp2 = 30
+wp3 = 50
+
+inp = str(input("ทำไรดี กด 1 แล้วบวกเลย หรือ กด 2 แล้วหนีไปซะะ :"))
+inp2 = str(input("เลือกอาวุธ :"))
+inp3 = str(input("เลือกจำนวนการโจมตี :"))
+
+# while inp == "1":
+#     print(inp3)
+#     print(inp2)
+#     if inp == 2:
+#         break
+
