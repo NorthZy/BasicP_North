4d0990ff2b02f9acd1a9fa037d7c3865284c9bb7
Student <Student@Train11-2-PC363.control.ad.sit.kmutt.ac.th>
2025-07-23 09:04:51 +0700

    Workshop

--- Class2_Workshop.py
+++ Class2_Workshop.py
@@ -7,7 +7,7 @@ elif int(delivery) <= int(100):
     print("price = 15 baht")
 elif int(delivery) <= int(300):
     print("price = 25 baht")
-elif int(delivery) <= int(499):
+elif int(delivery) <= int(500):
     print("price = 35 baht")
 else:
     print("price 45 baht")
